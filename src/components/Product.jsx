9c7e5b188d4867ecc92aa574e2eae5f87a9f431b
azadtom <kumarazad2917@gmail.com>
2023-12-23 13:17:56 +0530

    add loading

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,14 +6,14 @@ const Product=({title,description,price,image})=>{
     return(
         <>
          <div>
-            <div className="max-w-[300px] w-full">
-                <img src={image} alt="heroImg" className="w-full h-full" />
+            <div>
+                <img src={image} alt="heroImg" className="bg-center bg-cover object-cover" />
             </div>
             <div className="flex flex-col gap-2 p-4  bg-black">
                 <h2 className="text-xl">{title}</h2>
                 <p className="text-sm font-thin">{description}</p>
                 <span>{`$${price}`}</span>
-                <button  className="px-4 py-2 bg-white text-black">Add to cart</button>
+                <button  className="px-4 py-2 bg-white text-black"    >Add to cart</button>
             </div>
          </div>
         </>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,6 +9,8 @@ const Products = () => {
 
     const [data, setData] = useState([]);
 
+    const [loading,setLoading]= useState(true);
+
 
 
 
@@ -21,13 +23,14 @@ const Products = () => {
         const response = await axios.get("https://fakestoreapi.com/products");
 
         setData(response.data);
+        setLoading(false);
 
         
        } catch (error) {
         
 
          console.log(error.message);
-         
+
        }
 
     }
@@ -40,13 +43,27 @@ const Products = () => {
     useEffect(() => {
 
         fetchData();
+
     }, [])
+
+
+
+    if(loading)
+    {
+        return(
+            <>
+            <div className="flex justify-center items-center h-screen w-full">
+                <span>Loading...</span>
+            </div>
+            </>
+        )
+    }
 
     return (
         <>
             <div className="flex flex-col  gap-4 m-4 justify-center items-center">
                 <h1 className="text-2xl font-semibold">Products</h1>
-                <div className="grid grid-cols-3 justify-between  gap-4 max-w-[1000px]"> 
+                <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 justify-between  gap-4 max-w-[1000px]"> 
                     {data.map((item) => (
 
                         <Product {...item} key={item.id} />
